f1da4f2f7549482a857177a6f65d59a403193b13
Leslie Mwangi <luciannahphoi@gmail.com>
2024-03-25 14:40:50 +0300

    index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-function scuberGreetingForFeet(ride){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
   if (ride <= 400) {
-    return "This one is on me!"
+    return "This one is on me!";
   } else if (ride > 400 && ride <= 2000) {
-    return "That will be twenty bucks."
+    return "That will be twenty bucks.";
   } else if (ride > 2000 && ride <= 2500) {
-    return "I will gladly take your thirty bucks."
+    return "I will gladly take your thirty bucks.";
   } else {
     return "No can do.";
   }
 }
 
-function ternaryCheckCity(destination){
+function ternaryCheckCity(destination) {
   // Write your code here!
-  return destination !== 'NYC' ? 'No go.' : 'Ok, sounds good.'
+  return destination !== "NYC" ? "No go." : "Ok, sounds good.";
 }
 
-function switchOnCharmFromTip(tip){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
   if (tip === "generous") {
-    return "Thank you so much."
+    return "Thank you so much.";
   } else if (tip === "not as generous") {
-    return "Thank you."
+    return "Thank you.";
   } else {
-    return "Bye."
+    return "Bye.";
   }
 }
